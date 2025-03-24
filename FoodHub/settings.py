4d0db92c6bb080d1f64b6f166fdb537b70a78df4
kinin01 <nk21abl@herts.ac.uk>
2025-03-24 09:55:08 +0000

    fix

--- FoodHub/settings.py
+++ FoodHub/settings.py
@@ -175,7 +175,11 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-ALLOWED_HOSTS = ['foodhubbackend-700b.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['foodiehub-727v.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['foodiehub-727v.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['foodiehub-backend.onrender.com
+
+
 
 
 CORS_ALLOW_ALL_ORIGINS = True
